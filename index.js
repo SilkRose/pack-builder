1dc331a8065880195aab5dd18b0a1801b33c5432
VelvetRemedy <velvetremedy@love-tolerance.com>
2022-05-18 06:09:39 -0400

    fix json reading, add radio btn and for loop
    update url to live, add ;

--- index.js
+++ index.js
@@ -1,26 +1,38 @@
-const get_json = fetch("https://raw.githubusercontent.com/VelvetRemedy/pack-release-builder/mane/assets.json")
+const get_json = fetch("https://raw.githubusercontent.com/VelvetRemedy/pack-builder-server/mane/assets.json")
     .then((response) => response.json())
     .then((user) => {
         return user.repos;
-    });
+    }
+);
 
 const generate_options = async () => {
     const assets = await get_json;
     document.querySelector("#builder").innerHTML = "";
     for (c in assets.addons) {
-        console.log(assets.addons[c]);
+        document.querySelector("#builder").innerHTML += `<div id="${assets.addons[c].name}"></div>`;
+        for (a in assets.addons[c].variants) {
+            if (assets.addons[c].default == assets.addons[c].variants[a].id) {
+                document.querySelector(`#${assets.addons[c].name}`).innerHTML += `<div>
+                    <input type="radio" value="" name="${assets.addons[c].name}" checked="true"><p>${assets.addons[c].variants[a].name}</p></input>
+                </div>`;
+            } else {
+                document.querySelector(`#${assets.addons[c].name}`).innerHTML += `<div>
+                    <input type="radio" value="" name="${assets.addons[c].name}"><p>${assets.addons[c].variants[a].name}</p></input>
+                </div>`;
+            }
+        }
     }
 };
 
 function load_assets() {
     generate_options();
-}
+};
 
 function log(msg) {
     document.getElementById("log").textContent += msg + "\n";
-}
+};
 
-var ws = new WebSocket("ws://localhost:8080/");
+var ws = new WebSocket("ws://mc.love-tolerance.com:24808/");
 ws.onopen = function () {
     log("CONNECT");
 };
@@ -32,9 +44,7 @@ ws.onmessage = function (event) {
 };
 
 function sendMessage() {
-    var msgtxt = document.getElementById("id").value;
-    var test = JSON.stringify(obj);
-    ws.send(test);
-}
+    ws.send("Pinkie Pie is best pony!");
+};
 
 load_assets();
